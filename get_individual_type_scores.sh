96b92ac71cca77883c05c3ae9680f93d5ada51be
Jose Rosales <josecar25@gmail.com>
2021-02-05 10:19:39 +0100

    commit

--- get_individual_type_scores.sh
+++ get_individual_type_scores.sh
@@ -1,3 +1,3 @@
-python calculate_BLEU_SB_vs_MBD.py  --experiment types --MT_system s2s --number_tirages 1000
-python calculate_BLEU_SB_vs_MBD.py  --experiment types --MT_system c2c --number_tirages 1000
-python calculate_BLEU_SB_vs_MBD.py  --experiment types --MT_system TX --number_tirages 1000
+python scripts/calculate_BLEU_SB_vs_MBD.py  --experiment types --MT_system s2s --number_tirages 1000
+python scripts/calculate_BLEU_SB_vs_MBD.py  --experiment types --MT_system c2c --number_tirages 1000
+python scripts/calculate_BLEU_SB_vs_MBD.py  --experiment types --MT_system TX --number_tirages 1000
