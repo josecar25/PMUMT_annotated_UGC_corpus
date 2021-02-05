62cead1221bf480921be8c18591882ff34bde92b
Jose Rosales <josecar25@gmail.com>
2021-02-05 10:15:52 +0100

    commit

---
+++ get_N_types_scores.sh
@@ -0,0 +1,3 @@
+python calculate_BLEU_SB_vs_MBD.py  --experiment ntypes --MT_system s2s --number_tirages 300
+python calculate_BLEU_SB_vs_MBD.py  --experiment ntypes --MT_system c2c --number_tirages 300
+python calculate_BLEU_SB_vs_MBD.py  --experiment ntypes --MT_system TX --number_tirages 300
