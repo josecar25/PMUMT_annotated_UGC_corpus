6d7b1e8ea1264001e59c45c5300c0042af7fbf33
Jose Rosales <josecar25@gmail.com>
2021-11-09 12:42:04 +0100

    commit

---
+++ generate_datasets_by_types.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p ./types/TX
+python ./scripts/extract_full_types_and_norm.py  --errors_file errors.fr --in_src norms.fr --in_ref norms.en --in_pred norms.pred_seq2seq.sust_unk.en
